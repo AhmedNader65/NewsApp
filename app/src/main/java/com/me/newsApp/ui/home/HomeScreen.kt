93b59f03a41ee8f9299cccf00927c5127eb93743
AhmedBW <anader@bluworks.io>
2023-12-04 22:57:55 +0200

    basic home ui

--- app/src/main/java/com/me/newsApp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/me/newsApp/ui/home/HomeScreen.kt
@@ -1,16 +1,36 @@
 package com.me.newsApp.ui.home
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.me.newsApp.domain.model.Article
 import com.me.newsApp.ui.theme.components.LoadingWheel
 
 @Composable
@@ -20,9 +40,71 @@ fun HomeScreen(
     LaunchedEffect(key1 = Unit, block = {
         viewModel.fetchArticles()
     })
-
+    Box {
+        HomeContent(
+            viewModel = viewModel,
+            onArticleClick = { /*...*/ }
+        )
+    }
     HandleState(viewModel)
 
+}
+
+@Composable
+fun HomeContent(viewModel: HomeViewModel, onArticleClick: () -> Unit) {
+    val articles = remember {
+        viewModel.articles
+    }
+    LazyColumn {
+        items(articles.size) { index ->
+            ArticleItem(
+                article = articles[index],
+                onArticleClick = onArticleClick
+            )
+        }
+    }
+}
+
+@Composable
+fun ArticleItem(article: Article, onArticleClick: () -> Unit) {
+    val painter =
+        rememberAsyncImagePainter(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(article.urlToImage)
+                .size(
+                    coil.size.Size.ORIGINAL
+                ) // Set the target size to load the image at.
+                .build()
+        )
+    com.me.newsApp.common.Logger.i("state:${painter.state}")
+    Card(
+        modifier = Modifier
+            .padding(vertical = 8.dp, horizontal = 16.dp),
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .clickable { onArticleClick() }
+        ) {
+            if (painter.state is AsyncImagePainter.State.Success) {
+                Image(painter = painter, contentDescription = "")
+            } else {
+                Box(
+                    modifier = Modifier
+                        .height(200.dp)
+                        .background(Color.Gray)
+                )
+            }
+            Text(
+                modifier= Modifier.padding(horizontal = 16.dp, vertical = 16.dp),
+                text = article.title,
+                textAlign = TextAlign.Start,
+                style = MaterialTheme.typography.titleSmall
+            )
+        }
+    }
 }
 
 @Composable
@@ -37,6 +119,7 @@ fun HandleState(viewModel: HomeViewModel) {
                     contentDesc = "loading",
                 )
             }
+
             is HomeUIState.Success -> {}
             is HomeUIState.Error -> {
                 Toast.makeText(
--- app/src/main/java/com/me/newsApp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/me/newsApp/ui/home/HomeViewModel.kt
@@ -1,9 +1,12 @@
 package com.me.newsApp.ui.home
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.me.newsApp.domain.model.AppErrors
+import com.me.newsApp.domain.model.Article
 import com.me.newsApp.domain.usecase.FetchTopHeadlines
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -22,6 +25,7 @@ class HomeViewModel @Inject constructor(
     private val _homeUiState: MutableStateFlow<HomeUIState> = MutableStateFlow(HomeUIState.Idle)
     val homeUiState: StateFlow<HomeUIState> = _homeUiState.asStateFlow()
 
+    var articles : SnapshotStateList<Article> = mutableStateListOf()
     fun fetchArticles() {
         viewModelScope.launch {
             _homeUiState.emit(HomeUIState.Loading)
@@ -33,6 +37,7 @@ class HomeViewModel @Inject constructor(
         return withContext(Dispatchers.IO) {
             try {
                 val result = invoke()
+                articles.addAll(result)
                 _homeUiState.value = HomeUIState.Idle
             } catch (e: AppErrors) {
                 _homeUiState.value = HomeUIState.Error(e.message ?: "Unknown error")
